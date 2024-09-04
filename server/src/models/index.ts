b2a53ebba12f3e1c2e152b9fe96f2c3a8d7511e6
Elaine Muhombe <emuhombe@gmail.com>
2024-09-04 23:54:39 +0300

    chore: Export User and Mark models/interfaces from their respective modules

---
+++ server/src/models/index.ts
@@ -0,0 +1,5 @@
+// Export the User model and IUser interface from the User module
+export { User, IUser } from './User';
+
+// Export the Mark model and IMark interface from the Mark module
+export { Mark, IMark } from './Mark';
